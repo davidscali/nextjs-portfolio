e3abed58f8edb0e1cc601a334c9246ae303eb5b7
davidscali <scali0506@gmail.com>
2024-02-19 19:04:31 +0200

    Chakara

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -10,7 +10,7 @@ function Footer() {
 
     const footerButtons = [
         { type: 'main', content: 'Email Me', onClick: () => window.open('mailto:scali0506@gmail.com') },
-        // { testid: 'Telegram', type: 'main', content: 'Telegram', onClick: () => window.open('https://telegram.me/yangxng') },
+        { testid: 'Telegram', type: 'main', content: 'Telegram', onClick: () => window.open('https://telegram.me/DavidScali') },
         {
             type: 'main', content: 'LinkedIn', onClick: () => window.open('https://www.linkedin.com/in/david-scali/')
         }
